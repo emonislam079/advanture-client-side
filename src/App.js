13c1f3d3334d8e42b39b5a9166a8b481d82828ac
emonislam079 <emonislam079@gmail.com>
2021-10-28 23:23:05 +0600

    amni

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Home from './Components/Home/Home/Home';
+import Header from './Components/Shared/Header/Header';
+import Footer from './Components/Shared/Footer/Footer';
+import About from './Components/About/About';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Button>button</Button>
+      <Router>
+        <Header></Header>
+        <Switch>
+        <Route exact path="/">
+              <Home></Home>
+          </Route>
+          <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/About">
+              <About></About>
+            </Route>
+
+
+
+
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
