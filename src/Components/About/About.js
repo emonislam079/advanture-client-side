74dda5b71abfb1aac8f9c75a6cade15856ee9734
emonislam079 <emonislam079@gmail.com>
2021-10-30 22:41:32 +0600

    about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about</h1>
+        <div className="mt-5 container">
+            <h1 className="mt-5 text-primary">About US</h1>
+            <br></br>
+            <h3 className="text-secondary">We listen to you. </h3>
+            <h3 className="text-secondary"> We understand exactly what you want your trip to be.</h3>
+            <h3 className="text-secondary">Then we make it happen.</h3>
+            <div className="text-start mt-5 ">
+            <h2>Why travel with Adventure</h2>
+            <p>Our specialists have all travelled extensively in their regions and they return each year to learn more to find new and different ways for you to experience your destination. That means that we can arrange immersive tours and off-the-beaten-path activities to help you get at the authentic soul of a place. We also work with the best local guides, some of whom are long-time friends of Audley, and can suggest the most interesting hotels, homestays and lodges.</p>
+            <h2>How it works</h2>
+            <p>Our experts listen to your interests, share their advice, and then handcraft a personalised itinerary based on what you want and nothing more. They'll work with you until you’re satisfied with every detail. We’ll then manage all your reservations and logistics so that you’re taken care of from the moment you step off the plane until your return. Whether you choose to include guides throughout or set off on a more independent trip, you can travel reassured, knowing that we have more than 20 years of experience and provide you with around-the-clock support to smooth out the unexpected.</p>
+            <h2>Meet our specialists</h2>
+            <p>It takes a certain type of individual to be an Audley specialist. They'll have a close connection to their countries, often through living, studying or travelling extensively there. But, more than that, they have a passion for travel running through their veins. They'll give you an honest take and share a love for their destination that's infectious. Wedded to this is a deep understanding of the practical considerations of your visit: the best ways to get around, the most rewarding routes and how to put together even the most seemingly complicated trips.</p>
+            </div>
         </div>
     );
 };
